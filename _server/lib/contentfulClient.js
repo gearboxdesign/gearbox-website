1e290b244053fed6b909b7a59272fb0512a84125
Matt Robinson <matt@gearboxdesign.co.uk>
2016-11-30 20:48:53 +0000

    Corrects syntax error in contentfulClient

--- _server/lib/contentfulClient.js
+++ _server/lib/contentfulClient.js
@@ -5,8 +5,8 @@ const contentful = require('contentful');
 const dev = process.env.NODE_ENV === 'development',
 	client = contentful.createClient({
 		space: process.env.CONTENTFUL_SPACE_ID,
-		accessToken: dev ? process.env.CONTENTFUL_API_KEY,
-		host: dev ? process.env.CONTENTFUL_HOST
+		accessToken: process.env.CONTENTFUL_API_KEY
+		host: process.env.CONTENTFUL_HOST
 	});
 
 module.exports = client;
