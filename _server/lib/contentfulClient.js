a6700981daf1e5005cfaf929ff3a63a1e18d6562
Matt Robinson <matt@gearboxdesign.co.uk>
2016-11-30 20:42:14 +0000

    Updates contentful host / api key configuration

--- _server/lib/contentfulClient.js
+++ _server/lib/contentfulClient.js
@@ -5,8 +5,8 @@ const contentful = require('contentful');
 const dev = process.env.NODE_ENV === 'development',
 	client = contentful.createClient({
 		space: process.env.CONTENTFUL_SPACE_ID,
-		accessToken: dev ? process.env.CONTENTFUL_PREVIEW_API_KEY : process.env.CONTENTFUL_PRODUCTION_API_KEY,
-		host: dev ? process.env.CONTENTFUL_PREVIEW_HOST : process.env.CONTENTFUL_PRODUCTION_HOST
+		accessToken: dev ? process.env.CONTENTFUL_API_KEY,
+		host: dev ? process.env.CONTENTFUL_HOST
 	});
 
 module.exports = client;
