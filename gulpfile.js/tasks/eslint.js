ce57b99b0f4438489fa1c5e5ac77e524fa501444
Matt Robinson <matt@gearboxdesign.co.uk>
2016-12-17 16:14:18 +0100

    Removes HTML reporter from eslint

--- gulpfile.js/tasks/eslint.js
+++ gulpfile.js/tasks/eslint.js
@@ -9,30 +9,17 @@ const eslint = require('gulp-eslint'),
 const pathJoin = require('utils/pathJoin'),
 	paths = require('config/paths');
 
-const dev = process.env.NODE_ENV === 'development',
-	src = [
-		pathJoin(paths.scripts.src, '**', '*.js'),
-		pathJoin(paths.server, '**', '*.js'),
-		pathJoin(paths.universal, '**', '*.js')
-	];
+const src = [
+	pathJoin(paths.scripts.src, '**', '*.js'),
+	pathJoin(paths.server, '**', '*.js'),
+	pathJoin(paths.universal, '**', '*.js')
+];
 
 function eslintTask () {
 
 	return gulp.src(src)
 		.pipe(eslint())
-		.pipe(dev ? eslint.format('html', (results) => {
-
-			try {
-				fs.mkdirSync(paths.reports);
-			}
-			catch (err) { /* console.log(err); */ }
-
-			const reportFile = path.join(paths.reports, 'eslint.html');
-
-			fs.writeFileSync(reportFile, results);
-			open(reportFile);
-
-		}) : eslint.format())
+		.pipe(eslint.format())
 		.pipe(eslint.failOnError());
 }
 
